ab06410a45f11c3b1243c5e6b84275fbdbdf987c
VitalyBibik <61142441+VitalyBibik@users.noreply.github.com>
2020-05-04 19:04:28 +0300

    initial

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1 class="header__logo"></h1>
+    <nav class="menu">
+      <a href="">link</a>
+      <a href="">link</a>
+      <a href="">link</a>
+      <a href="">link</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped></style>
---
+++ pages/stories/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="div">
+    all stories
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+};
+</script>
+
+<style scoped></style>
