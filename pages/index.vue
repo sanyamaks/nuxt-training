674b2b85515478f864c62089ffa65446ad8d74f8
Rim Sungatovich <rsungatovichsocial@gmail.com>
2020-05-10 15:45:34 +0300

    add MainVideo block

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <main-video></main-video>
     <main-stories></main-stories>
     <instagram></instagram>
     <about-us></about-us>
@@ -10,12 +11,14 @@
 import Instagram from '@/components/Instagram';
 import AboutUs from '@/components/AboutUs';
 import MainStories from '@/components/MainStories';
+import MainVideo from '@/components/MainVideo';
 
 export default {
   components: {
     'about-us': AboutUs,
     'main-stories': MainStories,
     instagram: Instagram,
+    'main-video': MainVideo,
   },
 };
 </script>
