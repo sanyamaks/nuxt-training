86b0d692de45c145b6dc48959259ba42c90e4f79
VitalyBibik <61142441+VitalyBibik@users.noreply.github.com>
2020-05-10 21:19:45 +0300

    Статистика Альфа

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <main-stories></main-stories>
     <banner>РАССКАЗЫВАЙТЕ ВАШИ ИСТОРИИ В ИНСТАГРАМ</banner>
     <instagram></instagram>
+    <statistika></statistika>
     <about-us></about-us>
   </main>
 </template>
@@ -13,13 +14,14 @@ import Banner from '@/components/Banner';
 import Instagram from '@/components/Instagram';
 import AboutUs from '@/components/AboutUs';
 import MainStories from '@/components/MainStories';
-
+import Statistika from '@/components/Statistika';
 export default {
   components: {
     'about-us': AboutUs,
     'main-stories': MainStories,
     banner: Banner,
     instagram: Instagram,
+    statistika: Statistika,
   },
 };
 </script>
